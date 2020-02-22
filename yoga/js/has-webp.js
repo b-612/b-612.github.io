556fd3f86842757f0953846789082c3580f05b7c
b-612 <fromtheorion@yandex.ru>
2020-02-22 22:36:20 +0300

    Добавляет новую версию, но еще не доработанную до конца, сайта Йога

---
+++ yoga/js/has-webp.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  var hasWebP = function () {
+    var rv = $.Deferred();
+    var img = new Image();
+    img.onload = function() { rv.resolve(); };
+    img.onerror = function() { rv.reject(); };
+    img.src = 'img/test-webp.webp';
+    return rv.promise();
+  };
+
+  hasWebP().then(
+  function() {
+    $.noop();
+  },
+
+  function() {
+    $('.page-header').removeClass('webp').addClass('no-webp');
+    $('.directions').removeClass('webp').addClass('no-webp');
+    $('.subscriptions').removeClass('webp').addClass('no-webp');
+    $('.team').removeClass('webp').addClass('no-webp');
+  });
+})();
